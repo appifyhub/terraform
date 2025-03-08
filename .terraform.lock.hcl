0110353cd70880aa4fddac0291f2a3643d67030f
Milos Marinkovic <milosmns@gmail.com>
2025-03-08 22:49:08 +0100

    Add a basic Terraform config

---
+++ .terraform.lock.hcl
@@ -0,0 +1,104 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/http" {
+  version     = "3.4.5"
+  constraints = "~> 3.4.5"
+  hashes = [
+    "h1:eSVCYfvn5JyV3LC0+mrLlLtgLv4B+RWeNqz02miBcMY=",
+    "zh:2072006c177efc101471f3d5eb8e1d8e6c68778cbfd6db3d3f22f59cfe6ce6ae",
+    "zh:3ac4cc0efe11ee054300769cfcc37491433937a8824621d1f8f7a18e7401da87",
+    "zh:63997e5457c9ddf9cfff17bd7bf9f083cbeff3105452045662109dd6be499ef9",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:826819bb8ab7d6e3095f597083d5b1ab93d1854312b9e1b6c18288fff9664f34",
+    "zh:8ad74e7d8ec2e226a73d49c7c317108f61a4cb803972fb3f945d1709d5115fcd",
+    "zh:a609ca9e0c91d250ac80295e39d5f524e8c0872d33ba8fde3c3e41893b4b015d",
+    "zh:ae07d19babc452f63f6a6511b944990e819dc20687b6c8f01d1676812f5ada53",
+    "zh:b7c827dc32a1a5d77185a78cd391b01217894b384f58169f98a96d683730d8ce",
+    "zh:d045e3db9f5e39ce78860d3fd94e04604fcbe246f6fe346ee50a971f936e9ccd",
+    "zh:ec28f9b52c74edd47eebbb5c254a6df5706360cde5ccd65097976efca23a2977",
+    "zh:f24982eaa7d34fd66554c3cf94873713a0dff14da9ea4c4be0cc76f1a6146d59",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/local" {
+  version     = "2.5.2"
+  constraints = "~> 2.5.2"
+  hashes = [
+    "h1:IyFbOIO6mhikFNL/2h1iZJ6kyN3U00jgkpCLUCThAfE=",
+    "zh:136299545178ce281c56f36965bf91c35407c11897f7082b3b983d86cb79b511",
+    "zh:3b4486858aa9cb8163378722b642c57c529b6c64bfbfc9461d940a84cd66ebea",
+    "zh:4855ee628ead847741aa4f4fc9bed50cfdbf197f2912775dd9fe7bc43fa077c0",
+    "zh:4b8cd2583d1edcac4011caafe8afb7a95e8110a607a1d5fb87d921178074a69b",
+    "zh:52084ddaff8c8cd3f9e7bcb7ce4dc1eab00602912c96da43c29b4762dc376038",
+    "zh:71562d330d3f92d79b2952ffdda0dad167e952e46200c767dd30c6af8d7c0ed3",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:805f81ade06ff68fa8b908d31892eaed5c180ae031c77ad35f82cb7a74b97cf4",
+    "zh:8b6b3ebeaaa8e38dd04e56996abe80db9be6f4c1df75ac3cccc77642899bd464",
+    "zh:ad07750576b99248037b897de71113cc19b1a8d0bc235eb99173cc83d0de3b1b",
+    "zh:b9f1c3bfadb74068f5c205292badb0661e17ac05eb23bfe8bd809691e4583d0e",
+    "zh:cc4cbcd67414fefb111c1bf7ab0bc4beb8c0b553d01719ad17de9a047adff4d1",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/random" {
+  version     = "3.7.1"
+  constraints = "~> 3.7.0"
+  hashes = [
+    "h1:t152MY0tQH4a8fLzTtEWx70ITd3azVOrFDn/pQblbto=",
+    "zh:3193b89b43bf5805493e290374cdda5132578de6535f8009547c8b5d7a351585",
+    "zh:3218320de4be943e5812ed3de995946056db86eb8d03aa3f074e0c7316599bef",
+    "zh:419861805a37fa443e7d63b69fb3279926ccf98a79d256c422d5d82f0f387d1d",
+    "zh:4df9bd9d839b8fc11a3b8098a604b9b46e2235eb65ef15f4432bde0e175f9ca6",
+    "zh:5814be3f9c9cc39d2955d6f083bae793050d75c572e70ca11ccceb5517ced6b1",
+    "zh:63c6548a06de1231c8ee5570e42ca09c4b3db336578ded39b938f2156f06dd2e",
+    "zh:697e434c6bdee0502cc3deb098263b8dcd63948e8a96d61722811628dce2eba1",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:a0b8e44927e6327852bbfdc9d408d802569367f1e22a95bcdd7181b1c3b07601",
+    "zh:b7d3af018683ef22794eea9c218bc72d7c35a2b3ede9233b69653b3c782ee436",
+    "zh:d63b911d618a6fe446c65bfc21e793a7663e934b2fef833d42d3ccd38dd8d68d",
+    "zh:fa985cd0b11e6d651f47cff3055f0a9fd085ec190b6dbe99bf5448174434cdea",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/time" {
+  version     = "0.12.1"
+  constraints = "~> 0.12.1"
+  hashes = [
+    "h1:JzYsPugN8Fb7C4NlfLoFu7BBPuRVT2/fCOdCaxshveI=",
+    "zh:090023137df8effe8804e81c65f636dadf8f9d35b79c3afff282d39367ba44b2",
+    "zh:26f1e458358ba55f6558613f1427dcfa6ae2be5119b722d0b3adb27cd001efea",
+    "zh:272ccc73a03384b72b964918c7afeb22c2e6be22460d92b150aaf28f29a7d511",
+    "zh:438b8c74f5ed62fe921bd1078abe628a6675e44912933100ea4fa26863e340e9",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:85c8bd8eefc4afc33445de2ee7fbf33a7807bc34eb3734b8eefa4e98e4cddf38",
+    "zh:98bbe309c9ff5b2352de6a047e0ec6c7e3764b4ed3dfd370839c4be2fbfff869",
+    "zh:9c7bf8c56da1b124e0e2f3210a1915e778bab2be924481af684695b52672891e",
+    "zh:d2200f7f6ab8ecb8373cda796b864ad4867f5c255cff9d3b032f666e4c78f625",
+    "zh:d8c7926feaddfdc08d5ebb41b03445166df8c125417b28d64712dccd9feef136",
+    "zh:e2412a192fc340c61b373d6c20c9d805d7d3dee6c720c34db23c2a8ff0abd71b",
+    "zh:e6ac6bba391afe728a099df344dbd6481425b06d61697522017b8f7a59957d44",
+  ]
+}
+
+provider "registry.terraform.io/hetznercloud/hcloud" {
+  version     = "1.50.0"
+  constraints = "~> 1.50.0"
+  hashes = [
+    "h1:txFVTG8RI/X3NnyR1RST2cYZiseoDsCVjZt/v2Z6Q/I=",
+    "zh:0bd650fb52e272f74eda5053a7bb62f0fd92182f57ad3ef742abe165cb8cac98",
+    "zh:1c36667aa89b672a96c0df3d3c613e80916a2d0944b1a1f9112065f40630b689",
+    "zh:21f90683890ea7a184b0ac55efd52911694ba86c58898bc8bbe87ee2507bb1eb",
+    "zh:24349d483a6ff97420d847433553fa031f68f99b9ead4ebb3592fc8955ef521f",
+    "zh:3fffd83c450bea2b382a986501ae51a4d3e6530eda48ed9ca74d518e4a909c37",
+    "zh:43d7de1dc4c50fae99d6c4ab4bb394608948091f5b53ddb29bc65deead9dc8a6",
+    "zh:47a37d5fec79dd8bc9cab2c892bc59e135b86cb51eebe2b01cdb40afac7ed777",
+    "zh:6efeb9530b8f57618c43f0b294b983d06cce43e9423bdd737eed81db913edb80",
+    "zh:7511ace4b33baddfc452ef95a634d83b92bfbfaa23cb30403899e95b64727075",
+    "zh:7bade77104ed8788c9b5171c7daae6ab6c011b3c40b152274fda803bf0bf2707",
+    "zh:83bce3ff9a1bd52a340a6ebdd2e2b731ec6fb86811ef0ed8a8264daf9d7beb61",
+    "zh:a09d5fce4c8d33e10b9a19318c965076db2d8ed5f62f5feb3e7502416f66d7bf",
+    "zh:c942832b80270eb982eeb9cc14f30a437db5fd28faf37d6aa32ec2cd345537d6",
+    "zh:e2c1812f2e1f9fac17c7551d4ab0efb713b6d751087c18b84b8acd542f587459",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,193 @@
+# Terraform language documentation: https://www.terraform.io/docs/language/index.html
+# HCL language specification: https://github.com/hashicorp/hcl/blob/main/hclsyntax/spec.md
+# Module documentation: https://registry.terraform.io/modules/identiops/k3s/hcloud/latest
+# Copyright 2024, identinet GmbH. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+###########################
+#    I M P O R T A N T    #
+###########################
+
+# Go to `cluster_init_action` in this file to enable/disable init depending
+# on whether this is the first `tf apply` run or a subsequent one.
+
+###########################
+#  Backend and Providers  #
+###########################
+
+terraform {
+  # See Version Constraints: https://developer.hashicorp.com/terraform/language/expressions/version-constraints
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+  # Example s3 configuration (not needed yet):
+  # backend "s3" {
+  #   bucket = "xxxxx-terraform"
+  #   key    = "prod/terraform.tfstate"
+  #   # access_key                  = {}
+  #   # secret_key                  = {}
+  #   # skip_get_ec2_platforms      = true
+  #   region                      = "eu-central-2"
+  #   skip_credentials_validation = true
+  #   skip_metadata_api_check     = true
+  #   skip_region_validation      = true
+  #   skip_requesting_account_id  = true
+  #   skip_s3_checksum            = true
+  #   use_path_style              = true
+  #   endpoints = {
+  #     iam = "https://iam.eu-central-2.wasabisys.com" # special endpoint URL required, see https://wasabi-support.zendesk.com/hc/en-us/articles/360003362071-How-do-I-use-Terraform-with-Wasabi-
+  #     sts = "https://sts.eu-central-2.wasabisys.com" # special endpoint URL required, see https://wasabi-support.zendesk.com/hc/en-us/articles/360003362071-How-do-I-use-Terraform-with-Wasabi-
+  #     s3  = "https://s3.eu-central-2.wasabisys.com"  # special endpoint URL required, see https://wasabi-support.zendesk.com/hc/en-us/articles/360003362071-How-do-I-use-Terraform-with-Wasabi-
+  #   }
+  # }
+  required_version = "~> 1.0"
+}
+
+###########################
+#  Cluster configuration  #
+###########################
+
+module "cluster" {
+  # source               = "github.com/identiops/terraform-hcloud-k3s?ref=5.0.0"
+  source                 = "identiops/k3s/hcloud"
+  version                = "5.0.0"
+  hcloud_token           = var.hcloud_token           # Set via `export TF_VAR_hcloud_token=xyz` or in .tfvars
+  hcloud_token_read_only = var.hcloud_token_read_only # Set via `export TF_VAR_hcloud_token_read_only=abc` or in .tfvars
+
+  # Cluster Settings
+  # ----------------
+  delete_protection = false # Must be set to false + `terraform apply` before destroying the cluster via `terraform destroy`!
+  cluster_name      = "ah"
+  default_location  = "fsn1"         # See all: https://registry.terraform.io/providers/hetznercloud/hcloud/latest/docs/resources/server#location
+  default_image     = "ubuntu-24.04" # See all: `curl -H \"Authorization: Bearer $HETZNER_API_TOKEN_READONLY\" https://api.hetzner.cloud/v1/images | jq -r '.images[].name' | sort`
+  k3s_version       = "v1.32.2+k3s1" # See all: https://update.k3s.io/v1-release/channels; Regular images: https://hub.docker.com/r/rancher/k3s/tags; Upgrade images: https://hub.docker.com/r/rancher/k3s-upgrade/tags
+
+  # General Settings
+  # ----------------
+  # Root access to all systems
+  ssh_keys = {
+    "admin" = file("~/.ssh/id_rsa.pub")
+  }
+
+  # Just kubeapi access via port-forwarding
+  ssh_keys_kubeapi = {
+    "admin" = file("~/.ssh/id_rsa.pub")
+  }
+
+  # Gateway Settings
+  # ----------------------
+  gateway_firewall_k8s_open = true  # Open kubeapi port to the internet
+  gateway_server_type       = "cx22" # See all: https://docs.hetzner.com/cloud/servers/overview#shared-vcpu
+  gateway_labels            = {
+    "gateway" = "yes"
+  }
+
+  # Control Plane Settings
+  # ----------------------
+  # Example s3 configuration (not needed yet):
+  # S3 documentation  https://docs.k3s.io/cli/server
+  # control_plane_k3s_init_additional_options = "--etcd-s3 --etcd-s3-region=${var.etcd_s3_region} --etcd-s3-endpoint=s3.${var.etcd_s3_region}.wasabisys.com --etcd-s3-access-key=${var.etcd_s3_access_key} --etcd-s3-secret-key=${var.etcd_s3_secret_key} --etcd-s3-bucket=${var.etcd_s3_bucket} --etcd-s3-folder=etcd/$(hostname)"
+  # ETCD tuning documentation for multi-region deployment (not needed yet): https://etcd.io/docs/v3.4/tuning/#time-parameters
+  # control_plane_k3s_additional_options      = "--etcd-arg=heartbeat-interval=120 --etcd-arg=election-timeout=1200" # See https://etcd.io/docs/v3.4/tuning/#time-parameters
+  additional_cloud_init = {
+    timezone = "Europe/Berlin" # See all: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones#List
+  }
+
+  # Node Group Settings
+  # -------------------
+  # Map of worker node groups, key is server_type, value is count of nodes in group
+  node_pools = {
+    system = {
+      cluster_can_init = true # Required for one node pool to perform initializing actions.
+      cluster_init_action = {
+        # `init` must be `true` for the first run of `terraform apply`.
+        # For later runs it should be set to `false` to prevent any accidential
+        # reinitialization of the cluster, e.g. when the first node of this pool
+        # is manually deleted via the management console.
+        init = false,
+      }
+      is_control_plane   = true
+      schedule_workloads = true
+      type               = "cx22" # See all: https://docs.hetzner.com/cloud/servers/overview#shared-vcpu
+      count              = 1 # Should be an odd number for etcd quorum
+      count_width        = 2
+      labels = {
+        "control-plane" = "yes"
+      }
+      taints = {
+        # "MyTaint=true" = "NoSchedule"
+      }
+    }
+    workers = {
+      is_control_plane   = false
+      schedule_workloads = true
+      type               = "cx22" # See all: https://docs.hetzner.com/cloud/servers/overview#shared-vcpu
+      count              = 1
+      count_width        = 2
+      labels             = {
+        "worker-node" = "yes"
+      }
+      taints             = {}
+    }
+  }
+}
+
+
+###############
+#  Variables  #
+###############
+
+variable "hcloud_token" {
+  description = "Hetzner cloud auth token."
+  type        = string
+  sensitive   = true
+}
+
+variable "hcloud_token_read_only" {
+  description = "Hetzner cloud auth token, read only - used by the gateway and all cluster servers to proxy kubernetes traffic to control plane nodes."
+  type        = string
+  sensitive   = true
+}
+
+# Example s3 configuraiton (not needed yet):
+# variable "etcd_s3_region" {
+#   type      = string
+#   sensitive = true
+# }
+# 
+# variable "etcd_s3_access_key" {
+#   type      = string
+#   sensitive = true
+# }
+# 
+# variable "etcd_s3_secret_key" {
+#   type      = string
+#   sensitive = true
+# }
+# 
+# variable "etcd_s3_bucket" {
+#   type      = string
+#   sensitive = true
+# }
+
+############
+#  Output  #
+############
+
+output "gateway" {
+  depends_on  = [module.cluster]
+  description = "IP Addresses of the gateway."
+  value       = module.cluster.gateway
+}
+
+output "node_pools" {
+  depends_on  = [module.cluster]
+  description = "IP Addresses of the worker node pools."
+  value       = module.cluster.node_pools
+}
+
+output "total_monthly_costs" {
+  depends_on  = [module.cluster]
+  description = "Total monthly costs for running the cluster."
+  value       = module.cluster.total_monthly_costs
+}
